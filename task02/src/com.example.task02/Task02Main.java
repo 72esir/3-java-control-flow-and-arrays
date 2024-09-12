b1107b918dabcea2d6ea1590fcfeedd47e626d86
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-12 12:44:51 +0500

    Саламатин Алексей.task2

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -1,15 +1,40 @@
 package com.example.task02;
-
+import java.util.Arrays;
 public class Task02Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         System.out.println(getSeason(12));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int[] vinter = {12,1,2};
+        int[] spring = {3,4,5};
+        int[] summer = {6,7,8};
+        int[] autumn = {9,10,11};
+        String res = "";
+        for (int i = 0; i < 3; i++){
+            if (vinter[i] == monthNumber){
+                res = "зима";
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            if (spring[i] == monthNumber){
+                res = "весна";
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            if (summer[i] == monthNumber){
+                res = "лето";
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            if (autumn[i] == monthNumber){
+                res = "осень";
+            }
+        }
+
+
+        return res;//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
