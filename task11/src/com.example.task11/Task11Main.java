e9589942a39bb2333174b4ff265442773ff29aaf
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-12 15:18:14 +0500

    Саламатин Алексей.task11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,25 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        if (arr == null || arr.length == 0)
+        {
+            return;
+        }
+
+        int min = arr[0];
+        int num = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] <= min){
+                min = arr[i];
+                num = i;
+            }
+        }
+        int temp = arr[0];
+        arr[0] = arr[num];
+        arr[num] = temp;
+
     }
 
 }
